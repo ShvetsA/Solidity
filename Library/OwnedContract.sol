21ab6853ef2887394eee38c7680e4fa0c67852ce
Shvets Anton <ashvets321@gmail.com>
2018-01-24 21:42:31 +0200

    Update OwnedContract.sol

--- Library/OwnedContract.sol
+++ Library/OwnedContract.sol
@@ -1,29 +1,28 @@
 // ----------------------------------------------------------------------------
- 68 // Owned contract
- 69 // ----------------------------------------------------------------------------
- 70 contract Owned {
- 71     address public owner;
- 72     address public newOwner;
- 73 
- 74     event OwnershipTransferred(address indexed _from, address indexed _to);
- 75 
- 76     function Owned() public {
- 77         owner = msg.sender;
- 78     }
- 79 
- 80     modifier onlyOwner {
- 81         require(msg.sender == owner);
- 82         _;
- 83     }
- 84 
- 85     function transferOwnership(address _newOwner) public onlyOwner {
- 86         newOwner = _newOwner;
- 87     }
- 88     function acceptOwnership() public {
- 89         require(msg.sender == newOwner);
- 90         OwnershipTransferred(owner, newOwner);
- 91         owner = newOwner;
- 92         newOwner = address(0);
- 93     }
- 94 }
- 95 
+  // Owned contract
+  // ----------------------------------------------------------------------------
+ contract Owned {
+     address public owner;
+      address public newOwner;
+  
+      event OwnershipTransferred(address indexed _from, address indexed _to);
+  
+     function Owned() public {
+          owner = msg.sender;
+      }
+  
+     modifier onlyOwner {
+          require(msg.sender == owner);
+         _;
+     }
+  
+      function transferOwnership(address _newOwner) public onlyOwner {
+          newOwner = _newOwner;
+     }
+      function acceptOwnership() public {
+         require(msg.sender == newOwner);
+         OwnershipTransferred(owner, newOwner);
+          owner = newOwner;
+         newOwner = address(0);
+      }
+ 

85dd1293f2841b26feb4703c1a9e2f5fac46120f
Shvets Anton <ashvets321@gmail.com>
2018-01-24 21:41:34 +0200

    Update SafeMath.sol

--- Library/SafeMath.sol
+++ Library/SafeMath.sol
@@ -1,21 +1,21 @@
 // ----------------------------------------------------------------------------
- 18 // Safe maths
- 19 // ----------------------------------------------------------------------------
- 20 library SafeMath {
- 21     function add(uint a, uint b) internal pure returns (uint c) {
- 22         c = a + b;
- 23         require(c >= a);
- 24     }
- 25     function sub(uint a, uint b) internal pure returns (uint c) {
- 26         require(b <= a);
- 27         c = a - b;
- 28     }
- 29     function mul(uint a, uint b) internal pure returns (uint c) {
- 30         c = a * b;
- 31         require(a == 0 || c / a == b);
- 32     }
- 33     function div(uint a, uint b) internal pure returns (uint c) {
- 34         require(b > 0);
- 35         c = a / b;
- 36     }
- 37 }
+  // Safe maths
+ // ----------------------------------------------------------------------------
+ library SafeMath {
+     function add(uint a, uint b) internal pure returns (uint c) {
+         c = a + b;
+          require(c >= a);
+     }
+      function sub(uint a, uint b) internal pure returns (uint c) {
+         require(b <= a);
+         c = a - b;
+     }
+      function mul(uint a, uint b) internal pure returns (uint c) {
+         c = a * b;
+         require(a == 0 || c / a == b);
+     }
+     function div(uint a, uint b) internal pure returns (uint c) {
+         require(b > 0);
+          c = a / b;
+     }
+ }

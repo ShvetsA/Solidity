0738901a0675cd3cdb4526d338e6066d61dac460
Shvets Anton <ashvets321@gmail.com>
2018-01-24 21:33:57 +0200

    Update FixedSupplyToken.sol

--- Library/FixedSupplyToken.sol
+++ Library/FixedSupplyToken.sol
@@ -124,4 +124,4 @@
      function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
          return ERC20Interface(tokenAddress).transfer(owner, tokens);
      }
-224 }
+ }
